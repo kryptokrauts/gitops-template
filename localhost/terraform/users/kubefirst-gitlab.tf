8f8c3c716a058de2f278364ca5f355baa46fd103
Thiago Pagotto <pagottoo@gmail.com>
2023-01-20 17:48:36 -0300

    fix local_dns instead aws_ placeholder to localhost templates
    Signed-off-by: Thiago Pagotto <pagottoo@gmail.com>

--- localhost/terraform/users/kubefirst-gitlab.tf
+++ localhost/terraform/users/kubefirst-gitlab.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "<TF_STATE_BUCKET>"
     key     = "terraform/users/tfstate.tf"
-    region  = "<AWS_DEFAULT_REGION>"
+    region  = "<LOCAL_DNS>"
     encrypt = true
   }
   required_providers {
